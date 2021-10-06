d2a37624f8e81b3d641f14fc2860b20c9fd2cfae
Alice-63 <alicetindag84@gmail.com>
2021-10-06 20:41:36 +0200

    add

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
-<?php
-
-$person="Ali Cetindag";
-
-echo "Persons Name: $person <br/>";
-echo 'Persons Name: $person';
-
-
-
-
-
-
-
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p id="result"></p>
+    are you online?
+    <button onclick="add()">Check</button>
+</body>
+</html>
