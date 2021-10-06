e095125e4bc3813f47beff4aaa61ec20565ba674
Alice-63 <alicetindag84@gmail.com>
2021-10-06 22:59:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,19 @@ $num1=10;
 $num2=50;
 
 
-echo $plus=$num1+$num2."<br/>";
-echo $minus=$num2-$num1."<br/>";
-echo $num1*$num2."<br/>";
-echo $num1/$num2."<br/>";
-echo $num2%$num1."<br/>";
-echo $num2==$num1
+if($num2>$num2)
+echo "Hallo";
+
+
+
+else
+
+
+
+echo "Leider"
+
+// else{
+//     echo "Leider"
+// }
 
 ?>
