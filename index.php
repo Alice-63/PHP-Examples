d6c31c22de299947d0ae4353969e37d5f7b45691
Alice-63 <alicetindag84@gmail.com>
2021-10-07 22:54:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,14 @@
 <?php
 
-$persons=array(
-    "Person"=>array(
-    "name"=> "Veli",
-    "surname"=> "Kayik", 
-    "age"=>"42"),
-
-    "Person2"=>array(
-        "name"=> "Ali",
-         "surname"=> "Cetindag", 
-         "age"=>"37")
-
-    );
-
-
-    print_r($persons["Person"]["name"]);
 
 
 
+function kareAl( $sayi) {
+   
+    echo $sayi*$sayi;
+}
+
+kareAl(20)
 
 
 
