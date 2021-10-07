6814ca284aebeeba709183d5a2eb5afed42d1236
Alice-63 <alicetindag84@gmail.com>
2021-10-07 11:29:27 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,10 @@
 echo $_POST["personelAdi"]. "<br/>". $_POST["email"];
 
 
-
 ?>
+
+<form action="index.php" method="POST">
+    <input type="text" name="personelAdi">
+    <input type="text" name="email" id="">
+    <button type="submit">Gönder</button>
+</form>
