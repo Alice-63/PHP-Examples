4c616fe5ae4d93ea89b63b11e3500a64fdced20f
Alice-63 <alicetindag84@gmail.com>
2021-10-07 12:48:46 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,15 @@
 
 
 
-$sayi=8;
-do{
-    echo $sayi."<br>";
-    $sayi++;
-}while($sayi==9)
+$names=array("ali","Veli","Kemal","Ahmet");
 
+print_r($names);
+
+echo "<hr>";
+
+foreach($names as $name=> $value)
+
+echo "$value <br>"
 
 ?>
 
