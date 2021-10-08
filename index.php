04f69866e8422e434ab618c4ca07b75cf98d494b
Alice-63 <alicetindag84@gmail.com>
2021-10-08 22:42:14 +0200

    add

--- index.php
+++ index.php
@@ -33,6 +33,16 @@ $persons=array(
         "isim"=>"Ali",
         "soyad"=>"Cetin",
         "age"=>37
+    ),
+    array=(
+        "isim"=>"Mustafa",
+        "soyad"=>"Cetin",
+        "age"=>10
+    ),
+    array=(
+        "isim"=>"Meryem",
+        "soyad"=>"Cetin",
+        "age"=>7
     )
 )
 ?>
--- server.php
+++ server.php
@@ -1,11 +1,6 @@
 <?php
 
-$num1=10;
-$num2=50;
 
-
-echo $plus=$num1+$num2;
-echo $minus=$num2-$num1;
 
 
 ?>
