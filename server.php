02ad577f5c04cf332544537ac106df649bc8aa15
Alice-63 <alicetindag84@gmail.com>
2021-10-08 22:42:42 +0200

    Update server.php

--- server.php
+++ server.php
@@ -1,6 +1,24 @@
 <?php
 
 
+$persons=array(
+    array=(
+        "isim"=>"Ali",
+        "soyad"=>"Cetin",
+        "age"=>37
+    ),
+    array=(
+        "isim"=>"Mustafa",
+        "soyad"=>"Cetin",
+        "age"=>10
+    ),
+    array=(
+        "isim"=>"Meryem",
+        "soyad"=>"Cetin",
+        "age"=>7
+    )
+)
+
 
 
 ?>
