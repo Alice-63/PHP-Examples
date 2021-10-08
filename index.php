6068c467228e553a6c464fa0dee35786b986fcbe
Alice-63 <alicetindag84@gmail.com>
2021-10-08 22:21:32 +0200

    add

--- index.php
+++ index.php
@@ -1,19 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="POST" id="myForm">
+        <input type="text" name="personelAdi">
+        <input type="text" name="email" id="">
+        <button type="submit" onclick="add()">
+        <?php
+        add(5)
+        
+        ?>
+        
+        </button>
+    </form>
+
+    <script>
+        function add(){
+           console.log(document.getElementById("myForm").submit())
+        }
+    </script>
+</body>
+</html>
 <?php
 
-
-
-
-function kareAl( $sayi) {
-   
-    return $sayi*$sayi;
-   
+function add(el)
+{
+    return el*el;
 }
-
-$sayi2=4;
-$toplam=$sayi2+ kareAl(20);
-echo $toplam
-
-
-
 ?>
-
