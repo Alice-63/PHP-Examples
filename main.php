8e14ee1888d45c8ab4922004deb79a288f291bee
Alice-63 <alicetindag84@gmail.com>
2021-10-12 20:51:39 +0200

    add

---
+++ main.php
@@ -0,0 +1,3 @@
+<div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates numquam architecto laborum, mollitia illum veniam iure id aspernatur sed porro possimus at iusto esse nam et, commodi iste blanditiis est?
+</div>
---
+++ menu.php
@@ -0,0 +1,5 @@
+<div>
+    <a href="">Anasayfa</a>
+    <a href="">Hakkimizda</a>
+    <a href="">Yeni</a>
+</div>
