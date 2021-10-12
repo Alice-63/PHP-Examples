92403e76b46317ed7339fc6776351b646d6e1414
Alice-63 <alicetindag84@gmail.com>
2021-10-12 21:39:58 +0200

    Update yeni.php

--- yeni.php
+++ yeni.php
@@ -3,6 +3,5 @@ include("menu.php")
 ?>
 <div>
     <h2>Yeni  ne varsa burda</h2>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque aspernatur officia ratione pariatur soluta. 
-        Harum libero quisquam non dolores, aliquam repellendus tenetur illo iste, ipsa aliquid sunt natus perferendis enim.</p>
+ 
 </div>
