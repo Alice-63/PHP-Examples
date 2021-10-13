5a02af3084752f9516fd63de582825b2f27efa72
Alice-63 <alicetindag84@gmail.com>
2021-10-13 23:23:41 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,7 @@
 session_start();
 
 
-// $_SESSION["UrunAdi"] = "Monitor";
-// $_SESSION["marka"] = "Simens";
-// $_SESSION["boyut"] = "22";
-// $_SESSION["Type"] = "led";
-// $_SESSION["id"] = "01";
 
 
-print_r($_SESSION);
+
+print_r($_SESSION)
