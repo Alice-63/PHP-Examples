db5912f22579f1fb736c47fd846ea9efecf7ffa8
Alice-63 <alicetindag84@gmail.com>
2021-10-15 21:45:06 +0200

    Update basket.php

--- basket.php
+++ basket.php
@@ -1,10 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Shopping Cart</title>
+</head>
+<body>
 
-session_start();
-echo "<pre>";
-print_r($_SESSION);
-echo "</pre>";
+
+<table>
+    <tbody>
+        <th>Product Name</th>
+        <th>Product Name</th>
+        <th>Product Name</th>
+        <th>Product Name</th>
 
 
-
-?>
+    </tbody>
+</table>
+    
+</body>
+</html>
