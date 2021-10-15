05314c810f86dfa7103b2d01fa222a0f031b6e1d
Alice-63 <alicetindag84@gmail.com>
2021-10-15 22:01:55 +0200

    Update basket.php

--- basket.php
+++ basket.php
@@ -5,6 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Shopping Cart</title>
+    <style>
+        table
+        {
+            width: 600px;
+            height: 600px;
+        }
+    </style>
 </head>
 <body>
 
