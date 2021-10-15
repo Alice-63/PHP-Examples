2f982139b0eed2247674d99412de9093db75efce
Alice-63 <alicetindag84@gmail.com>
2021-10-15 20:28:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,4 +4,15 @@
 
 
 ?>
-<form action="product.php"></form>
+<form action="product.php">
+    <select name="product" id="">
+
+        <option value="bildschrim" name="" id="">Bildschrim</option>
+        <option value="laptop" name="" id="">Laptop</option>
+        <option value="macbook" name="" id="">Macbook</option>
+        <option value="mouse" name="" id="">Mouse</option>
+    </select>
+    <input type="text" name="price" placeholder="Count" value="1">
+    <input type="text" name="count"  placeholder="Price" value="1500">
+    
+</form>
