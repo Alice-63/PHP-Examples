a6127fe747d319737afc8c0574add07d803d10c5
Alice-63 <alicetindag84@gmail.com>
2021-10-17 22:08:48 +0200

    Update products.php

--- products.php
+++ products.php
@@ -33,19 +33,7 @@ if($_GET){
 ?>
 <form action="products.php">
  <fieldset>
-     <h2>Order Form</h2>
-     <label for="products"><h3>Products List</h3></label><br>
-    <select name="product" id="products">
 
-        <option value="Bildschrim" name="" id="">Bildschrim</option>
-        <option value="Laptop" name="" id="">Laptop</option>
-        <option value="Macbook" name="" id="">Macbook</option>
-        <option value="Mouse" name="" id="">Mouse</option>
-        <option value="Notebook" name="" id="">Notebook</option>
-        <option value="Keyboard" name="" id="">Keyboard</option>
-    </select>
-    <input type="text" name="price" placeholder="Count" >
-    <input type="text" name="count"  placeholder="Price" >
     <button type="submit">Add</button>
  </fieldset>
 </form>
