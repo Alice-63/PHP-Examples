9ae02e3adf3e0467d7952e84353178a4d1e19b79
Alice-63 <alicetindag84@gmail.com>
2021-10-20 21:12:22 +0200

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,6 @@
 <?php
 
 
-try{$db=new PDO("mysql:host=localhost;dbname=uni;charset=utf8","root","");}
-catch(PDOException $e){
-    echo "<strong>$e->getCode() </strong>: $e->getMessage()";
-};
+
 
 ?>
