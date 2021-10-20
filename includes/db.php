5e4e05e4412d0c079e9cf2939b52c292e7d98b2f
Alice-63 <alicetindag84@gmail.com>
2021-10-20 21:50:52 +0200

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -5,7 +5,17 @@ try{
 $db= new PDO("mysql:host=localhost;dbname=personel;charset=utf8","root","");
 }catch(PDOException $e){
     echo $e->getMessage();
-}
+};
 
+$row=$db->query("SELECT * FROM personel", PDO::FETCH_ASSOC);
+
+ if( $row->rowCount()>0){
+
+    foreach($rows as $row)
+    {
+        print_r($row);
+        echo "<hr>";
+    }
+ };
 
 ?>
