e65108b27f88fb7e476bf12441508a9f94d012d9
Alice-63 <alicetindag84@gmail.com>
2021-10-21 15:21:48 +0200

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -7,16 +7,25 @@ $db= new PDO("mysql:host=localhost;dbname=person;charset=utf8","root","");
     echo $e->getMessage();
 };
 
-$row=$db->query("SELECT * FROM personel_list", PDO::FETCH_ASSOC);
+// $row=$db->query("SELECT * FROM personel_list", PDO::FETCH_ASSOC);
 
-foreach ($row as $rows ) {
+// foreach ($row as $rows ) {
   
-    echo "Personelin Adi: ". $rows["first_name"]."<br>";
-    echo "Personelin Soyadi: ". $rows["last_name"]."<br>";
-    echo "<hr>";
+//     echo "Personelin Adi: ". $rows["first_name"]."<br>";
+//     echo "Personelin Soyadi: ". $rows["last_name"]."<br>";
+//     echo "<hr>";
  
-}
+// }
 
+$id=10;
+$rows=$db -> query("SELECT * FROM personel_list where id=10", PDO::FETCH_ASSOC);
+
+foreach ($rows as $row ) {
+  
+    echo "Personelin Adi: ". $row["first_name"]."<br>";
+    echo "Personelin Soyadi: ". $row["last_name"]."<br>";
+    echo "<hr>";
+}
 
  
 
