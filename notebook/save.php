c99d18474f05392b21036f43f0fc6f74b16c692f
Alice-63 <alicetindag84@gmail.com>
2021-10-23 12:52:57 +0200

    Update save.php

--- notebook/save.php
+++ notebook/save.php
@@ -1,10 +1,30 @@
 <?php
 
-print_r ($_GET)
 
 
 
+if($_GET){
 
+$name=$_GET["name"];
+$lastname=$_GET["lastname"];
+$web=$_GET["web"];
+$email=$_GET["email"];
+$phone=$_GET["phone"];
+$facebook=$_GET["facebook"];
+$twitter=$_GET["twitter"];
+$birthday=$_GET["birthday"];
+$note=$_GET["note"];
+$adress=$_GET["adress"];
+
+
+try{
+$db=new PDO("mysql:host=localhosst;dbname=notbook;charset=utf8","root","");}
+catch(PDOException $e){
+    echo $e->getMessage();
+};
+
+
+}
 
 
 
