9e1248d8587ab5ca9fbe413069588b3ea23299e7
Alice-63 <alicetindag84@gmail.com>
2021-10-23 13:45:42 +0200

    Update addrecord.php

--- notebook/addrecord.php
+++ notebook/addrecord.php
@@ -17,36 +17,36 @@
             <form action="save.php" method="get">
             <div class="form-group">
                 <label for="">Name</label>
-                <input require type="text" class="form-control" name="name" placeholder="Name">
+                <input required type="text" class="form-control" name="name" placeholder="Name">
             </div>
             <div class="form-group">
                 <label for="">Lastname</label>
-                <input require type="text" class="form-control" name="lastname" placeholder="Lastname">
+                <input required type="text" class="form-control" name="lastname" placeholder="Lastname">
             </div>
             <div class="form-group">
                 <label for="">Web</label>
-                <input require type="text" class="form-control" name="web" placeholder="Web">
+                <input required type="text" class="form-control" name="web" placeholder="Web">
             </div>
             <div class="form-group">
                 <label for="">E-Mail</label>
-                <input require type="text" class="form-control" name="email" placeholder="E-Mail">
+                <input required type="text" class="form-control" name="email" placeholder="E-Mail">
             </div>
             <div class="form-group">
                 <label for="">Phone</label>
-                <input require type="number" class="form-control" name="phone" placeholder="Phone">
+                <input required type="number" class="form-control" name="phone" placeholder="Phone">
             </div>
             <div class="form-group">
                 <label for="">Facebook</label>
-                <input require type="text" class="form-control" name="facebook" placeholder="Facebook">
+                <input required type="text" class="form-control" name="facebook" placeholder="Facebook">
             </div>
             <div class="form-group">
                 <label for="">Twitter</label>
-                <input require type="text" class="form-control" name="twitter" placeholder="Twitter">
+                <input required type="text" class="form-control" name="twitter" placeholder="Twitter">
             </div>
          
             <div class="form-group">
                 <label for="">Birthday</label>
-                <input require type="date" class="form-control" name="birthday" placeholder="Birthday">
+                <input required type="date" class="form-control" name="birthday" placeholder="Birthday">
             </div>
             <div class="form-group">
                 <label for="">Note</label>
@@ -59,7 +59,7 @@
         
             </div>
         </div>
-       <button class="btn btn-primary mt-3"><a href="notebook.php" class="btn btn-primary mt-3">Save</a></button>
+        <a href="notebook.php">    <button class="btn btn-primary mt-3">Save</button></a>
         <a href="notebook.php" class="btn btn-danger mt-3">Cancel</a>
         </form>
     </div>
