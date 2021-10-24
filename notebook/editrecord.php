94f3b0b3bd89a0b1fda1b00379e41ccee3714c95
Alice-63 <alicetindag84@gmail.com>
2021-10-24 13:31:41 +0200

    Update editrecord.php

--- notebook/editrecord.php
+++ notebook/editrecord.php
@@ -31,44 +31,44 @@
             <form action="save.php" method="get">
             <div class="form-group">
                 <label for="">Name</label>
-                <input required type="text" class="form-control" name="name" placeholder="Name" value="<?php echo $row[""]<?">
+                <input required type="text" class="form-control" name="name" placeholder="Name" value="<?php echo $row["name"]?>">
             </div>
             <div class="form-group">
                 <label for="">Lastname</label>
-                <input required type="text" class="form-control" name="lastname" placeholder="Lastname" value="<?php echo $row[""]<?">
+                <input required type="text" class="form-control" name="lastname" placeholder="Lastname" value="<?php echo $row["lastname"]?>">
             </div>
             <div class="form-group">
                 <label for="">Web</label>
-                <input required type="text" class="form-control" name="web" placeholder="Web" value="<?php echo $row[""]<?">
+                <input required type="text" class="form-control" name="web" placeholder="Web" value="<?php echo $row["web"]?>">
             </div>
             <div class="form-group">
                 <label for="">E-Mail</label>
-                <input required type="text" class="form-control" name="email" placeholder="E-Mail" value="<?php echo $row[""]<?">
+                <input required type="text" class="form-control" name="email" placeholder="E-Mail" value="<?php echo $row["email"]?>">
             </div>
             <div class="form-group">
                 <label for="">Phone</label>
-                <input required type="number" class="form-control" name="phone" placeholder="Phone" value="<?php echo $row[""]<?">
+                <input required type="number" class="form-control" name="phone" placeholder="Phone" value="<?php echo $row["phone"]?>">
             </div>
             <div class="form-group">
                 <label for="">Facebook</label>
-                <input required type="text" class="form-control" name="facebook" placeholder="Facebook" value="<?php echo $row[""]<?">
+                <input required type="text" class="form-control" name="facebook" placeholder="Facebook" value="<?php echo $row["facebook"]?>">
             </div>
             <div class="form-group">
                 <label for="">Twitter</label>
-                <input required type="text" class="form-control" name="twitter" placeholder="Twitter" value="<?php echo $row[""]<?">
+                <input required type="text" class="form-control" name="twitter" placeholder="Twitter" value="<?php echo $row["twitter"]?>">
             </div>
          
             <div class="form-group">
                 <label for="">Birthday</label>
-                <input required type="date" class="form-control" name="birthday" placeholder="Birthday" value="<?php echo $row[""]<?">
+                <input required type="date" class="form-control" name="birthday" placeholder="Birthday" value="<?php echo $row["birthday"]?>">
             </div>
             <div class="form-group">
                 <label for="">Note</label>
-                <textarea rows="3" type="text" class="form-control" name="note" placeholder="Note" value="<?php echo $row[""]<?"></textarea>
+                <textarea rows="3" type="text" class="form-control" name="note" placeholder="Note" value="<?php echo $row["note"]?>"></textarea>
             </div>
             <div class="form-group">
                 <label for="">Adress</label>
-                <textarea rows="3"  type="text" class="form-control" name="adress" placeholder="Adress" value="<?php echo $row[""]<?"></textarea>
+                <textarea rows="3"  type="text" class="form-control" name="adress" placeholder="Adress" value="<?php echo $row["adress"]?>"></textarea>
             </div>
             <button class="btn btn-primary mt-3 float-start">Update</button>
             <a href="notebook.php" class="btn btn-danger mt-3 float-end">Cancel</a>
