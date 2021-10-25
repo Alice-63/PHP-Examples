095923f57ea3498fb4a51124bb59f2c278f354ff
Alice-63 <alicetindag84@gmail.com>
2021-10-25 21:40:29 +0200

    Update del.php

--- notebook/del.php
+++ notebook/del.php
@@ -9,12 +9,12 @@ $del->bindParam(":id",$id,PDO::PARAM_INT);
 
 $result=$del->execute();
 
-// if ($result){
-//     echo "Delete is successful!! <a href='notebook.php'>Click to List</a>";
-// }
-// else{
-//     echo "Delete is unsuccessful!! <a href='addrecord.php'>Try again</a>";
-// }
+if ($result){
+    echo "Delete is successful!! <a href='notebook.php'>Click to List</a>";
+}
+else{
+    echo "Delete is unsuccessful!! <a href='addrecord.php'>Try again</a>";
+}
 
 
 
