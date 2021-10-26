5e9c93fc3fe251cf5d1454661275530070534d9b
Alice-63 <alicetindag84@gmail.com>
2021-10-26 18:39:36 +0200

    Update del.php

--- notebook/del.php
+++ notebook/del.php
@@ -1,20 +1,3 @@
-<?php
-
-$id=$_GET["id"];
-
-include("../includes/db.php");
-
-$del=$db->prepare("DELETE FROM tbl_info WHERE id=:id");
-$del->bindParam(":id",$id,PDO::PARAM_INT);
-
-$result=$del->execute();
-
-if ($result){
-    echo "Delete is successful!! <a href='notebook.php'>Click to List</a>";
-}
-else{
-    echo "Delete is unsuccessful!! <a href='addrecord.php'>Try again</a>";
-}
 
 
 
