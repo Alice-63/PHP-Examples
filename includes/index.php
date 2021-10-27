81786110e427c7f062a0a617ef102b436a31d89e
Alice-63 <alicetindag84@gmail.com>
2021-10-27 19:33:10 +0200

    Update index.php

--- includes/index.php
+++ includes/index.php
@@ -34,19 +34,18 @@ $myarray=array(
 // echo "Name: ". $myarray["name"];
 
 // die();
-echo count($myarray);
-foreach($myarray as $key => $value)
+if($_GET)
 {
-    echo $key.": $value"."<br>";
+    echo $_GET["name"];
+    echo $_GET["sekil"];
 }
-
 
 ?>
 
 <body>
     <form action="" method="get">
         <input type="submit" name="name" value="1">
-        <input type="text" name="surname">
+        <input type="submit" name="sekil" value="*">
         <button type="submit">Click</button>
     </form>
 </body>
