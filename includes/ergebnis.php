33af6de3c62abb6fe44396c333c56c0b2a39769c
Alice-63 <alicetindag84@gmail.com>
2021-10-29 14:01:23 +0200

    add

--- includes/ergebnis.php
+++ includes/ergebnis.php
@@ -6,23 +6,23 @@ if($_GET)
    
     if($_GET["aktion"]=="+")
     {
-        $zeil1=($_GET["zahl1"].$_GET["aktion"].$_GET["zahl2"]."<br>");
-        $zeil2=($_GET["zahl1"]+$_GET["zahl2"]);
+        $zeil1=($_GET["num1"].$_GET["aktion"].$_GET["num2"]."<br>");
+        $zeil2=($_GET["num1"]+$_GET["num2"]);
     };
     if($_GET["aktion"]=="*")
     {
-        $zeil1=($_GET["zahl1"].$_GET["aktion"].$_GET["zahl2"]."<br>");
-        $zeil2=$_GET["zahl1"]*$_GET["zahl2"];
+        $zeil1=($_GET["num1"].$_GET["aktion"].$_GET["num2"]."<br>");
+        $zeil2=$_GET["num1"]*$_GET["num2"];
     };
     if($_GET["aktion"]=="-")
     {
-        $zeil1=($_GET["zahl1"].$_GET["aktion"].$_GET["zahl2"]."<br>");
-        $zeil2=$_GET["zahl1"]-$_GET["zahl2"];
+        $zeil1=($_GET["num1"].$_GET["aktion"].$_GET["num2"]."<br>");
+        $zeil2=$_GET["num1"]-$_GET["num2"];
     };
     if($_GET["aktion"]=="/")
     {
-        $zeil1=($_GET["zahl1"].$_GET["aktion"].$_GET["zahl2"]."<br>");
-        $number=$_GET["zahl1"]/$_GET["zahl2"];
+        $zeil1=($_GET["num1"].$_GET["aktion"].$_GET["num2"]."<br>");
+        $number=$_GET["num1"]/$_GET["num2"];
         $zeil2=number_format($number,2,'.',',');
     }
    
--- includes/index.php
+++ includes/index.php
@@ -14,15 +14,15 @@
         <form  action="ergebnis.php" method="get">
         <h2 class="mt-3 mb-3">Taschenrechner</h2>
             <select class="form-control form-control-lg" name="aktion" id="">   
-                <option value="+">Addieren +</option>
-                <option value="*">Multiplizieren *</option>
-                <option value="-">Subtrahieren -</option>
-                <option value="/">Dividieren /</option>
+                <option value="+">Add +</option>
+                <option value="*">Mult *</option>
+                <option value="-">Sub -</option>
+                <option value="/">Div /</option>
             </select>
-            <input required class="form-control form-control-lg mt-3" name="zahl1" type="text" placeholder="Zahl 1">
-            <input required class="form-control form-control-lg mt-3" name="zahl2" type="text" placeholder="Zahl 2">
+            <input required class="form-control form-control-lg mt-3" name="num1" type="text" placeholder="Number 1">
+            <input required class="form-control form-control-lg mt-3" name="num2" type="text" placeholder="Number 2">
 
-            <button  class="btn btn-warning btn-lg btn-block mt-3 w-100 form-control">Rechnen</button>
+            <button  class="btn btn-warning btn-lg btn-block mt-3 w-100 form-control">Calculate</button>
     
     
         </form>
