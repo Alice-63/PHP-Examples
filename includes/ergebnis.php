8550cab82aff46348d20c6f74100ef29fdf64271
Alice-63 <alicetindag84@gmail.com>
2021-10-29 11:24:43 +0200

    Update ergebnis.php

--- includes/ergebnis.php
+++ includes/ergebnis.php
@@ -6,7 +6,7 @@ if($_GET)
    
     if($_GET["aktion"]=="+")
     {
-        echo $_GET["zahl1"].$_GET["aktion"].$_GET["zahl2"];
+        echo $_GET["zahl1"].eval('return $_GET["aktion"]').$_GET["zahl2"];
     };
     if($_GET["aktion"]=="*")
     {
