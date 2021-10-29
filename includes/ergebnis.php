3f3a22655fe39f49d4c1842470b7fb6c54ad9061
Alice-63 <alicetindag84@gmail.com>
2021-10-29 11:38:55 +0200

    add

--- includes/ergebnis.php
+++ includes/ergebnis.php
@@ -6,8 +6,8 @@ if($_GET)
    
     if($_GET["aktion"]=="+")
     {
-        print($_GET["zahl1"]($_GET["aktion"])$_GET["zahl2"]);
-        echo ($_GET["zahl1"]+$_GET["zahl2"]);
+        $zeil1=($_GET["zahl1"].$_GET["aktion"].$_GET["zahl2"]."<br>");
+        $zeil2=($_GET["zahl1"]+$_GET["zahl2"]);
     };
     if($_GET["aktion"]=="*")
     {
@@ -27,3 +27,20 @@ if($_GET)
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php include("head.php")?>
+</head>
+<body>
+   <div class="container">
+   <h3><?php echo $zeil1?></h3>
+    <h3><?php echo $zeil2?></h3>
+    <button class="btn btn-primary">Clik</button>
+   </div>
+</body>
+</html>
