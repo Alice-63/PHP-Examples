dd332ee6f4f0cb222e0d0d729df208f40e988e7e
Alice-63 <alicetindag84@gmail.com>
2021-10-29 11:58:14 +0200

    Update index.php

--- includes/index.php
+++ includes/index.php
@@ -7,24 +7,25 @@
     <title>Document</title>
     <?php include("head.php")?>
 </head>
-<body class="bg bg-primary">
-<div class="container">
+<body class="bg bg-success">
+<div class="container-fluid">
 <div class="row">
-    <div class="col-6">
+    <div class="col-md-2">
     <form action="ergebnis.php" method="get">
     <div class="form-group">
     <h3>Aktion</h3>
         <select required class="form-control form-control-lg" name="aktion" id="">
-            <option value="null" >Wähl aus</option>    
+              
             <option value="+">Addieren +</option>
             <option value="*">Multiplizieren *</option>
             <option value="-">Subtrahieren -</option>
             <option value="/">Dividieren /</option>
         </select>
-   <input required class="form-control form-control-lg" name="zahl1" type="text" placeholder="Zahl 1">
-   <input required class="form-control form-control-lg" name="zahl2" type="text" placeholder="Zahl 2">
+   <input required class="form-control form-control-lg mt-3" name="zahl1" type="text" placeholder="Zahl 1">
+   <input required class="form-control form-control-lg mt-3" name="zahl2" type="text" placeholder="Zahl 2">
 
-   <button class="btn btn-primary">Rechnen</button>
+   <button type="button" class="btn btn-warning btn-lg btn-block mt-3 w-100">Rechnen</button>
+  
     </div>
 </form>
     </div>
