553340fad72a6eccf58d4d439978a99c0c2732be
Alice-63 <alicetindag84@gmail.com>
2021-10-29 14:19:54 +0200

    add

--- includes/index.php
+++ includes/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Taschenrechner</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body class="bg bg-success">
@@ -12,7 +12,7 @@
 <div class="row justify-content-md-center  ">
     <div class="col-md-2" >
         <form  action="ergebnis.php" method="get">
-        <h2 class="mt-3 mb-3">Taschenrechner</h2>
+        <h2 class="mt-3 mb-3 text-warning">Taschenrechner</h2>
             <select class="form-control form-control-lg" name="aktion" id="">   
                 <option value="+">Add +</option>
                 <option value="*">Mult *</option>
