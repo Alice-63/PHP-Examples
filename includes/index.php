6b47e91da3bd0c144755b2dbf5cb38f3f56d3226
Alice-63 <alicetindag84@gmail.com>
2021-10-30 10:00:49 +0200

    Update index.php

--- includes/index.php
+++ includes/index.php
@@ -11,21 +11,7 @@
 <div class="container-fluid ">
 <div class="row justify-content-md-center  ">
     <div class="col-md-2" >
-        <form  action="ergebnis.php" method="get">
-        <h2 class="mt-3 mb-3 text-warning">Taschenrechner</h2>
-            <select class="form-control form-control-lg" name="aktion" id="">   
-                <option value="+">Add +</option>
-                <option value="*">Mult *</option>
-                <option value="-">Sub -</option>
-                <option value="/">Div /</option>
-            </select>
-            <input required class="form-control form-control-lg mt-3" name="num1" type="text" placeholder="Number 1">
-            <input required class="form-control form-control-lg mt-3" name="num2" type="text" placeholder="Number 2">
-
-            <button  class="btn btn-warning btn-lg btn-block mt-3 w-100 form-control">Calculate</button>
-    
-    
-        </form>
+       
     </div>
 </div>
 </div>
