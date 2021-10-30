fcb2610c63f6997fa08c3c09f5d759fabda7917e
Alice-63 <alicetindag84@gmail.com>
2021-10-30 09:59:50 +0200

    Update ergebnis.php

--- includes/ergebnis.php
+++ includes/ergebnis.php
@@ -1,32 +1,6 @@
 <?php
 
 
-if($_GET)
-{
-   
-    if($_GET["aktion"]=="+")
-    {
-        $zeil1=($_GET["num1"].$_GET["aktion"].$_GET["num2"]."<br>");
-        $zeil2=($_GET["num1"]+$_GET["num2"]);
-    };
-    if($_GET["aktion"]=="*")
-    {
-        $zeil1=($_GET["num1"].$_GET["aktion"].$_GET["num2"]."<br>");
-        $zeil2=$_GET["num1"]*$_GET["num2"];
-    };
-    if($_GET["aktion"]=="-")
-    {
-        $zeil1=($_GET["num1"].$_GET["aktion"].$_GET["num2"]."<br>");
-        $zeil2=$_GET["num1"]-$_GET["num2"];
-    };
-    if($_GET["aktion"]=="/")
-    {
-        $zeil1=($_GET["num1"].$_GET["aktion"].$_GET["num2"]."<br>");
-        $number=$_GET["num1"]/$_GET["num2"];
-        $zeil2=number_format($number,2,'.',',');
-    }
-   
-}
 
 
 
