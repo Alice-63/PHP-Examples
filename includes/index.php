d855d54f71c8668e8ae63e604da9030398e561ba
Alice-63 <alicetindag84@gmail.com>
2021-10-30 10:04:17 +0200

    Update index.php

---
+++ includes/index.php
@@ -0,0 +1,26 @@
+<?php
+echo "hallooo"
+
+
+
+
+
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="background:darkorange;">
+    
+</body>
+</html>
