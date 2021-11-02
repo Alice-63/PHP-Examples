fa2425df91b031a4f187d44cabd181f64c6c89fb
Alice-63 <alicetindag84@gmail.com>
2021-11-02 19:34:25 +0100

    Update index.php

--- includes/index.php
+++ includes/index.php
@@ -15,16 +15,25 @@ function all($a){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php
+    include("head.php");
+    ?>
 </head>
 <body style="background:darkorange;">
-  <form action="">
-      <input type="text" placeholder="Name">
-      <input type="text" placeholder="Surname">
-      <input type="text" placeholder="E-Mail">
-      <input type="text" placeholder="Password">
-      <input type="submit" value="Konto Erstellen">
-  </form>
+  <div class="row">
 
-   <div><?php all(6);?></div>
+        <h1>Konto Erstellen</h1>
+      <div class="col-md-6 d-flex justify-content-center">
+      <form action="" class="form-group">
+      <input class="form-control" type="text" placeholder="Name">
+      <input class="form-control" type="text" placeholder="Surname">
+      <input class="form-control" type="text" placeholder="E-Mail">
+      <input class="form-control" type="text" placeholder="Password">
+      <input class="form-control" type="submit" value="Konto Erstellen">
+  </form>
+      </div>
+  </div>
+
+   
 </body>
 </html>
