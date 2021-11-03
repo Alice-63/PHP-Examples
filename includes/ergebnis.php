02683bccab924b6b47e4aa0310f0f4c7729cded2
Alice-63 <alicetindag84@gmail.com>
2021-11-03 18:21:37 +0100

    Update ergebnis.php

--- includes/ergebnis.php
+++ includes/ergebnis.php
@@ -13,7 +13,7 @@ $surname=$_POST["surname"];
 $email=$_POST["email"];
 $pass=$_POST["pass"];
 
-echo "Herzlichen Wilkommen $name <br> $surname <br>";
+echo "Herzlichen Wilkommen <b>$name</b> <br> $surname <br>";
 echo "Ihre E-Mail adresse ist : $email <br>";
 echo "Ihr Passwort :$pass <br>"
 ?>
