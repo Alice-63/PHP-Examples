3d8c1b27db40732c44d64bc6d9427c21823d3b47
Alice-63 <alicetindag84@gmail.com>
2021-11-04 19:50:57 +0100

    Update ergebnis.php

--- includes/ergebnis.php
+++ includes/ergebnis.php
@@ -18,5 +18,20 @@ $datenbankPass="";
 
 $conn=new mysqli($serverName,$datenbankUser,$datenbankPass);
 
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  
+
+$anfrage="INSERT INTO `users` (`id`, `vorname`, `nachname`, `email`, `passwort`) VALUES ('.$name.', '.$surname.', '.$email.', '.$pass.', '')";
+
+if($verbindung->query($anfrage)==true)
+{
+    echo "Connected successfully";
+}
+else{
+    echo "Connected unsuccessfull!";
+}
+
 
 ?>
