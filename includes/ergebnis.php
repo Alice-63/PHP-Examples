f01766d88631948cf3793c90ae1e571e8e328956
Alice-63 <alicetindag84@gmail.com>
2021-11-06 10:07:30 +0100

    Update ergebnis.php

--- includes/ergebnis.php
+++ includes/ergebnis.php
@@ -1,7 +1,8 @@
 <?php
 
 
-
+$datei=readfile("merhaba.txt");
+echo $datei."<br>";
 
 $name=$_POST["name"];
 $surname=$_POST["surname"];
@@ -21,18 +22,10 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
 
-  $data=array("name"=>$name,"surname"=>$surname,"email"=>$email,"pass"=>$pass);
 
+$anfrage="INSERT INTO users (vorname, nachname, email, passwort) VALUES ('".$name."', '".$surname."', '".$email."', '".$pass."')";
 
-
-$anfrage="INSERT INTO users SET (vorname=:name, nachname=:surname, email=:email, passwort=:pass)";
-$result=$anfrage->execute($data);
-
-
-
-
-
-if($conn->query($result)==true)
+if($conn->query($anfrage)==true)
 {
     echo "Connected successfully .<a href='index.php'>Neue Konto erstellen</a>";
 }
@@ -40,7 +33,5 @@ else{
     echo "Connected unsuccessfull!";
 }
 
-
-// VALUES ('".$name."', '".$surname."', '".$email."', '".$pass."')
 
 ?>
