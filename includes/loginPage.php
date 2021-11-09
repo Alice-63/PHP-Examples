7c70c67174307b79814e0d2faaa2efff2de83d24
Alice-63 <alicetindag84@gmail.com>
2021-11-09 18:40:32 +0100

    Update loginPage.php

---
+++ includes/loginPage.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=h1, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Anmelden</h1>
+    <form action="login.php"  method="post">
+    <input required  name="email" type="email">    
+    <input required name="passwort" type="text">
+    <button type="submit">Save</button>
+    </form>
+</body>
+</html>
