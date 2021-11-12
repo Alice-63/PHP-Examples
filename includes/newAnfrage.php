a08305ce4bb6afff2be79325b28027701fb2bd3c
Alice-63 <alicetindag84@gmail.com>
2021-11-12 12:59:42 +0100

    add

--- includes/newAnfrage.php
+++ includes/newAnfrage.php
@@ -1,6 +1,6 @@
 <?php
     echo $_GET["name"]." <br>".$_GET["surname"];
-    echo "<br><a href='newAnfragePage.php'>Click</a>";
+    echo "<br><a href='newAnfragePage.php'>Click</a><br>";
 
 $name=$_GET["name"];
 $surname=$_GET["surname"];
@@ -14,9 +14,11 @@ $pass="";
 
 $conn=new mysqli($server,$user,$pass,$datenbank);
 
-$anfrage="SELECT * FROM newtabel";
+$anfrage="SELECT * FROM newtabel WHERE name='$name'";
 
 $result=$conn->query($anfrage);
+$row=$result->fetch_assoc();
+print_r($row);
 
 while($row=$result->fetch_assoc())
 {
@@ -24,6 +26,9 @@ while($row=$result->fetch_assoc())
     if($row["name"]==$name)
     {
         echo "Wilkommen :".$name;
+    }
+    else{
+        echo "nicht Gefunden!";
     }
    }
 
