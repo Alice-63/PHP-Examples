227504be97edb61b7696a8f8d0e62a92d903262d
Alice-63 <alicetindag84@gmail.com>
2021-11-12 12:31:53 +0100

    add

--- includes/newResult.php
+++ includes/newResult.php
@@ -1,3 +1,33 @@
 <?php
 
-echo $_GET["name"]." <br>".$_GET["surname"];
+    echo $_GET["name"]." <br>".$_GET["surname"];
+    echo "<br><a href='newLogin.php'>Click</a>"
+
+$name=$_GET["name"];
+$surname=$_GET["surname"];
+
+
+$server="localhost";
+$datenbank="dk_teamprojekt";
+$user="root";
+$pass="";
+
+
+$conn=new mysqli($server,$datenbank,$user,$pass);
+
+$anfrage="INSERT INTO newtabel(name,surname) VALUES($name,$surname)";
+
+$result=$conn->query($anfrage);
+
+if($result)
+{
+    echo "Succsseful"
+}
+else{
+    echo "error!"
+}
+
+
+
+
+?>
