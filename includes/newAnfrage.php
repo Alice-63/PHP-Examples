fca1b03e9a0dbef7b145b39a311b97c646229a14
Alice-63 <alicetindag84@gmail.com>
2021-11-12 12:52:10 +0100

    add

--- includes/newAnfrage.php
+++ includes/newAnfrage.php
@@ -34,20 +34,3 @@ while($row=$result->fetch_assoc())
 
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="">
-    <input name="name" type="text">    
-    <input name="surname" type="text">
-    <input type="submit" name="" value="Anruffen">
-    </form>
-</body>
-</html>
