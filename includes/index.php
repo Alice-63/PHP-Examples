0af7d361799d08ad98cb634d06c83270ac607206
Alice-63 <alicetindag84@gmail.com>
2021-11-15 21:47:12 +0100

    add

--- includes/index.php
+++ includes/index.php
@@ -1,10 +1,7 @@
 <?php
 
-
-function all($a){
-    $b=$_GET["name"];
-    echo $a*$b;
-}
+$eingelogt=false;
+$name="";
 
 ?>
 
@@ -20,21 +17,22 @@ function all($a){
     ?>
 </head>
 <body style="background:black;">
-  <div class="row d-flex justify-content-center">
-
  
-      <div class="col-md-6 d-flex justify-content-center">
-      <form action="ergebnis.php" method="POST" class="form-group">
-      <h1>Konto Erstellen</h1>
-      <input required name="name" class="form-control" type="text" placeholder="Name">
-      <input required name="surname" class="form-control" type="text" placeholder="Surname">
-      <input required name="email" class="form-control" type="text" placeholder="E-Mail">
-      <input required name="pass" class="form-control" type="text" placeholder="Password">
-      <input required name="" class="form-control" type="submit" value="Konto Erstellen">
-  </form>
-      </div>
-  </div>
+<?php
+ if($eingelogt)
+ {
+?>
 
+<h1>Herzlichen Wilkkomen <?= $name?> bei Digitale Karierre</h1>
+
+<?php
+}
+else{
+    ?>
+    Bitte Registiriren! <a href="newLogin.php">Click</a>
+
+    <?php
+}?>
 
    
 </body>
