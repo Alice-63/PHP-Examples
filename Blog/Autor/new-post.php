01307a7cb4ffef7a3b4f9d3fa8b080336e93521f
Alice-63 <alicetindag84@gmail.com>
2021-11-19 23:52:30 +0100

    add

--- Blog/Autor/new-post.php
+++ Blog/Autor/new-post.php
@@ -19,13 +19,15 @@ include("../db.php");
         <textarea name="post_inhalt" placeholder="Inhalt"></textarea> <br>
         <select name="post_kategorie" id=""> <br>
             <?php
-            $anfrage="SELECT * FROM  category"
+            $anfrage="SELECT * FROM  category";
             $result=$db->query($anfrage);
-            $kat_row=$result->fetch_assoc();
             
             
+            while( $kat_row=$result->fetch_assoc()){
             ?>
-            
+            <option value="<?=$kat_row["id"]?>"><?=$kat_row["name"]?></option>
+            <?php
+            }?>
 
         </select>
         <input type="submit" name="new_post"value="Erstellen">
