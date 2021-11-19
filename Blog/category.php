028054cbee3f2aba06d6f4e33e500e7275516247
Alice-63 <alicetindag84@gmail.com>
2021-11-19 12:09:41 +0100

    add

---
+++ Blog/category.php
@@ -0,0 +1,13 @@
+<?php
+include("header.php");
+include("db.php");
+
+?>
+<main>
+    <h1>Willkommen in Kategorie<?=$kat_name?></h1>
+</main>
+<?php
+include("sidebar.php");
+include("footer.php");
+include("close.php");
+?>
