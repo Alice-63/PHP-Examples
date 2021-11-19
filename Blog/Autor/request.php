e5cd733446190a20d996d5dacefd66a3da31221e
Alice-63 <alicetindag84@gmail.com>
2021-11-20 00:38:43 +0100

    add

--- Blog/Autor/request.php
+++ Blog/Autor/request.php
@@ -9,7 +9,8 @@ else if(isset($_POST["new_post"]))
 {
     $inhalt=htmlspecialchars($_POST["post_inhalt"]);
     $date="2021.12.12";
-    $anfrage="INSERT INTO posts (titel,content,category,Autor) VALUES({$_POST["post_titel"]},{$inhalt},{$_POST["post_kategorie"]},1)";
+    $titel=$_POST["post_titel"];
+        $anfrage="INSERT INTO posts (titel,content,category,Autor) VALUES({$titel},{$inhalt},{$_POST["post_kategorie"]},1)";
     $result=$db->query($anfrage);
 
     if($result)
