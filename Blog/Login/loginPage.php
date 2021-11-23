0769c70f5c354835583aebaf26dcbc46683fd11e
Alice-63 <alicetindag84@gmail.com>
2021-11-23 16:33:05 +0100

    add

--- Blog/Login/loginPage.php
+++ Blog/Login/loginPage.php
@@ -1,10 +1,13 @@
-
-<?php
-
-
-?>
-
-    <main>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<main>
     <h1>Konto Erstellen</h1>
     <form action="login.php" method="get">
         <input name="benutzername" type="text" placeholder="Benutzername">
@@ -22,7 +25,6 @@
 
     ?>
     </main>
-<?php
+</body>
+</html>
 
-
-?>
